f88a01d35f8c1a6aeba43b0a2452112212155e54
vubangtran <tranbangfw206@gmail.com>
2026-02-03 23:32:21 +0700

    lab4

--- src/Lab/Lab4/Lab4.js
+++ src/Lab/Lab4/Lab4.js
@@ -2,17 +2,27 @@ const username_log = "admin";
 
 const password_log = "adminrights123";
 
+const usernameElement = document.getElementById("username");
+const passwordElement = document.getElementById("password");
+
 const btnElement = document.getElementById("submitBtn");
+
+btnElement.addEventListener('click', (e) => {
+    e.preventDefault();
+})
 
 btnElement.addEventListener('click', () => {
 
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+    const username = usernameElement.value;
+    const password = passwordElement.value;
 
-    console.log(username, password);
+    console.log(username.value, password.value);
     if (username === username_log && password === password_log) {
         alert("Đăng nhập thành công !");
+        window.location.href = "success.html";
     } else {
         alert("Đăng nhập thất bại. Username hoặc mật khẩu không đúng.");
+        usernameElement.style.borderColor = "red";
+        passwordElement.style.borderColor = "red";
     }
 })
